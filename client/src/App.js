1e3423022fd4335ca23121b8c0aa60109b85fcf0
Austin Cox <austin.ed.cox@gmail.com>
2020-05-02 21:11:08 -0700

    updated some styles

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,23 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import NoMatch from "./pages/NoMatch";
 import { Footer } from "./components/Footer";
-import { StoreProvider } from "./utils/GlobalState";
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="allContent">
-        <StoreProvider>
-          <div className="bg-white">
-            <Nav />
-          </div>
-          <Switch>
-            <Route exact path="/" component={About} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/contact" component={Contact} />
-            <Route component={NoMatch} />
-          </Switch>
-          <Footer />
-        </StoreProvider>
+        <div className="bg-white">
+          <Nav />
+        </div>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
       </div>
     </Router>
   );
